4f29111de3799544a1852be557779c191b5d08e0
Julien Armagnac <julien.tamil@gmail.com>
2022-04-15 10:24:19 +0200

    Envoi initial

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# THP10_w2d4_arrays-hashes
-THP week 2 day 4 : arrays and hashes practice
+# THP10_w2d4_hashes
+Work on arrays &amp; hashes
---
+++ lib/00_journalists.rb
@@ -0,0 +1,138 @@
+$journalists_handles = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def accueil
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Manipulations sur le Array \"$journalists_handles\" :"
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    
+    print "\n[ENTRÉE] POUR CONTINUER"
+    gets
+end
+
+def question1
+
+    #Déterminer le nombre de handles contenu dans le tableau
+    puts "\nQuestionn° 1 : Combien y a-t-il de handle dans cette array ?"
+
+    reponseByCount = $journalists_handles.count
+    reponseByLength = $journalists_handles.length
+
+    puts " > Le Array contient #{reponseByCount} handles (méthode count)."
+    puts " > Le Array contient #{reponseByLength} handles (méthode length)."
+
+
+
+end
+
+
+def question2
+
+    #Déterminer la handle le plus court du tableau
+    puts "\nQuestion n°2 : Quel est le handle le plus court de cette liste ?"
+    
+    reponse = $journalists_handles.min_by {|size| size.length}
+    puts "Le handle le plus court de cette liste est #{reponse}."
+end
+
+
+def question3
+    #Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère) ?
+    puts "\nQuestion n°3 : Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère) ?"
+
+    reponse =  $journalists_handles.select {|handle| handle.length == 6}.count
+    puts "Il y a #{reponse} handle contenant 5 caractères (@ exclus)."
+end
+
+
+
+def question4
+    #Combien commencent par une majuscule (première lettre juste après le @) ?
+    puts "\nQuestion n°4 : Combien commencent par une majuscule (première lettre juste après le @) ?"
+
+    reponse = $journalists_handles.select {|handle| handle.split(//)[1] == handle.split(//)[1].upcase}.count 
+    puts "#{reponse} handle commencent par une majuscule."
+end
+
+def question5
+    #Trie la liste de handle par ordre alphabétique.
+    puts "\nQuestion n°5 : Trie la liste de handle par ordre alphabétique."
+    print "[ENTRÉE POUR LANCER LE TRI]"
+    gets
+    puts $journalists_handles.sort
+end
+
+def question6    
+    #Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après).
+    puts "\nQuestion n°6 : Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)."
+    
+    print "[ENTRÉE POUR LANCER LE TRI]"
+    gets    
+    puts $journalists_handles.sort_by {|handle| handle.length} 
+     
+end
+
+def question7
+    #Quelle est la position dans l'array de la personne @epenser ?
+    puts "\nQuestion n°7 : Quelle est la position dans l'array de la personne @epenser ?"
+
+    reponse = $journalists_handles.index("@epenser")
+    puts " > \"@epenser\" est à l'index #{reponse}."
+end
+
+
+def question8
+    #Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+    puts "\nQuestion n°8 : Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)."
+
+    #Il nous faut d'abord connaître et stocker toutes la taille des handle (dans un tableau allHandlesSizes)
+    allHandlesSizes = Array.new
+
+    $journalists_handles.each do |handle|
+        allHandlesSizes.push(handle.size)
+    end
+
+    #Création d'un tableau qui contient toutes les tailles mais SANS DOUBLONS, et classé dans l'ordre croissant pour faciliter la lecture
+    allHandlesSizes_uniq = allHandlesSizes.uniq.sort
+
+    #Affichage de la répartition des tailles de handles.
+    for i in 0..allHandlesSizes_uniq.length-1
+        reponse = allHandlesSizes.count(allHandlesSizes_uniq[i])
+        puts " > Il y a #{reponse} handle de taille #{allHandlesSizes_uniq[i]}."
+    end
+
+end
+
+def nextQuestion
+    #Permet de faire une pause dans le défilement des questions
+    print "\n[ENTRÉE] POUR LA QUESTION SUIVANTE"
+    gets
+end
+
+def perform
+    accueil
+
+    question1
+    nextQuestion
+
+    question2
+    nextQuestion
+
+    question3
+    nextQuestion
+
+    question4
+    nextQuestion
+
+    question5
+    nextQuestion
+
+    question6
+    nextQuestion
+
+    question7
+    nextQuestion
+
+    question8
+end
+
+perform
